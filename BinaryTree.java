a7738423b399f4b5aff786ad113f4ba55527ea4f
Sneha Sharma <snehassharma3003@gmail.com>
2026-03-25 18:06:52 +0530

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class BinaryTree {
+    TreeNode root;
+
+    void inorder(TreeNode node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+
+        tree.inorder(tree.root);
+    }
+}
